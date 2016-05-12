75b4409ee69fee778dd42e5be5a4c63b2076ec21
Miquel Sonsona <miquelsonsona@gmail.com>
2016-05-12 04:47:44 +0200

    getting game page url

--- src/get_goals.py
+++ src/get_goals.py
@@ -10,8 +10,15 @@ for jornada in soup.find_all('div', class_='jornadaCalendario'):
     num_jornada = jornada.div.h2.text.split()[1]
     print(jornada.div.h2.text)
     partidos = jornada.ul.find_all('li')
+    
     for partido in partidos:
         print(partido.h3.a['title'])
         local = partido.h3.a.find('span', class_='local').text
         visitante = partido.h3.a.find('span', class_='visitante').text
         resultado = partido.h3.a.find('span', class_='resultado').text
+        
+        if partido.h3.a.has_attr('href'):
+            partido_url = partido.h3.a['href']
+            print(partido_url)
+        else:
+            print("No url")
