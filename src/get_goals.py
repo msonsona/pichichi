9fb7cf01a6f601573df97badd4def97997ce734d
Miquel Sonsona <miquelsonsona@gmail.com>
2016-05-12 04:23:27 +0200

    listing all games

---
+++ src/get_goals.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get('http://www.marca.com/futbol/primera/calendario.html')
+soup = BeautifulSoup(page.content, 'html.parser')
+
+for jornada in soup.find_all('div', class_='jornadaCalendario'):
+    num_jornada = jornada.div.h2.text.split()[1]
+    print(jornada.div.h2.text)
+    partidos = jornada.ul.find_all('li')
+    for partido in partidos:
+        print(partido.h3.a['title'])
+        local = partido.h3.a.find('span', class_='local').text
+        visitante = partido.h3.a.find('span', class_='visitante').text
+        resultado = partido.h3.a.find('span', class_='resultado').text
